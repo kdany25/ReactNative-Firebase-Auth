b8a31db54a27dd308463a9012f6676ab85786c97
kdany25 <kabadany25@gmail.com>
2023-02-27 16:40:00 +0200

    feat: added Login screen

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,12 +1,172 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+	View,
+	Text,
+	ImageBackground,
+	StyleSheet,
+	TextInput,
+	Dimensions,
+	TouchableOpacity,
+	ScrollView,
+} from "react-native";
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <View>
-      <Text>Login</Text>
-    </View>
-  )
-}
+	const [email, setEmail] = useState("");
+	const [pass, setPass] = useState("");
+	const [rePass, setRePass] = useState("");
+	const [isRegister, setIsRegister] = useState(false);
+	const [isLogin, setIsLogin] = useState(false);
 
-export default Login
+	const handleLogin = async () => {};
+
+	const handleRegister = async () => {};
+	return (
+		<ImageBackground
+			source={require("../assets/bg-3.png")}
+			resizeMode="cover"
+			style={styles.container}
+		>
+			{isLogin ? (
+				<>
+					<Text>{email}</Text>
+
+					<TouchableOpacity onPress={() => {}}>
+						<Text>Logout</Text>
+					</TouchableOpacity>
+				</>
+			) : (
+				<ScrollView style={{ flex: 1 }}>
+					<Text
+						style={{
+							fontWeight: "bold",
+							fontSize: 28,
+							color: "#212121",
+							marginVertical: 20,
+						}}
+					>
+						{isRegister ? "Sign in" : "Login"}
+					</Text>
+
+					<View style={styles.loginFormContainer}>
+						<View style={styles.inputContainer}>
+							<Text style={styles.inputLabel}>Email</Text>
+							<TextInput
+								style={styles.input}
+								placeholder="email"
+								value={email}
+								onChangeText={(val) => setEmail(val)}
+								maxLength={100}
+								keyboardType="email-address"
+								autoCapitalize="none"
+								autoComplete="email"
+							/>
+						</View>
+
+						<View style={styles.inputContainer}>
+							<Text style={styles.inputLabel}>Password</Text>
+							<TextInput
+								style={styles.input}
+								placeholder="Password"
+								autoCapitalize="none"
+								autoComplete="off"
+								maxLength={100}
+								value={pass}
+								onChangeText={(val) => setPass(val)}
+								secureTextEntry
+							/>
+						</View>
+
+						{isRegister && (
+							<View style={styles.inputContainer}>
+								<Text style={styles.inputLabel}>Confirm</Text>
+								<TextInput
+									style={styles.input}
+									placeholder="confirm password"
+									autoCapitalize="none"
+									autoComplete="off"
+									maxLength={100}
+									value={rePass}
+									onChangeText={(val) => setRePass(val)}
+									secureTextEntry
+								/>
+							</View>
+						)}
+					</View>
+
+					<View style={{ height: 20 }} />
+
+					<TouchableOpacity
+						style={styles.buttonLogin}
+						onPress={isRegister ? handleRegister : handleLogin}
+					>
+						<Text style={styles.buttonText}>
+							{isRegister ? "Sign in" : "Login"}
+						</Text>
+					</TouchableOpacity>
+
+					<TouchableOpacity
+						onPress={() => setIsRegister(!isRegister)}
+						style={styles.buttonRegister}
+					>
+						<Text>{isRegister ? "Login" : "Register"}</Text>
+					</TouchableOpacity>
+				</ScrollView>
+			)}
+		</ImageBackground>
+	);
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+	container: {
+		padding: 20,
+		flex: 1,
+	},
+
+	loginFormContainer: {
+		width: Dimensions.get("window").width - 40,
+	},
+
+	inputContainer: {
+		marginTop: 8,
+		marginBottom: 16,
+	},
+
+	inputLabel: {
+		fontWeight: "500",
+		color: "#212121",
+		fontSize: 16,
+	},
+
+	input: {
+		flex: 1,
+		backgroundColor: "#fafafa",
+		marginTop: 8,
+		padding: 10,
+		borderRadius: 100,
+		minHeight: 40,
+	},
+
+	buttonLogin: {
+		height: 40,
+		backgroundColor: "coral",
+		width: Dimensions.get("window").width - 40,
+		justifyContent: "center",
+		alignItems: "center",
+		borderRadius: 100,
+	},
+
+	buttonText: {
+		fontWeight: "bold",
+		fontSize: 18,
+		textTransform: "uppercase",
+		color: "#fafafa",
+	},
+
+	buttonRegister: {
+		marginVertical: 10,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+});
